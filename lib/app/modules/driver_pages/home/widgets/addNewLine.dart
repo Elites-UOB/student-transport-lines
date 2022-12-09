164ff0679007c1b4b49dbbbebcc81f8b241f1ca0
LIINWARE <97309058+ShahadJawad@users.noreply.github.com>
2022-12-09 21:00:24 +0300

    homedriver

---
+++ lib/app/modules/driver_pages/home/widgets/addNewLine.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:student_transport_lines/core/theme/color_theme.dart';
+import 'package:student_transport_lines/core/theme/text_theme.dart';
+
+class addNewLine extends StatelessWidget {
+  const addNewLine({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+  final w = MediaQuery.of(context).size.width;
+  final h =MediaQuery.of(context).size.height;
+    return  ListTile(
+
+          //image
+          leading:Container(
+              height:60.sp ,width: 42.52.sp,
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child:const Image(image: AssetImage(''),
+              ) ),
+          //collage name
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+             const Text('كلية علوم الحاسوب / صباحي',style: Styles.boldblue,),
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children:const [
+                    Text('4',style: Styles.boldblue,),
+                    SizedBox(width: 25,),
+                    Text('2',style: Styles.boldblue),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+
+          //university name
+          subtitle: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+             const Text('جامعة البصرة'),
+              Row(
+                children: const[
+                  Text('مقاعد ',style: Styles.hintText,),
+                  SizedBox(width: 5,),
+                  Text('شاغر',style: Styles.hintText)
+                ],
+              )
+
+            ],
+          ),
+
+    );
+
+
+
+
+  }
+}
+
+
+// //مقاعد && شاغر
+// trailing:Row(
+// mainAxisAlignment: MainAxisAlignment.end,
+// children: [
+// Column(
+// children: [
+// Text('كلية علوم الحاسوب / صباحي')
+// ],
+// ),
+//
+// Column(
+// children: const[
+// Text('4',
+// style: TextStyle(
+// color: Palette.blueColor,fontWeight: FontWeight.bold,fontSize: 18
+// ),),
+// Text(' مقاعد   ',style: Styles.hintText,)
+// ],
+// ),
+//
+// Column(
+// children: const[
+// Text('2',
+// style: TextStyle(
+// color: Palette.blueColor,fontWeight: FontWeight.bold,fontSize: 18
+// ),),
+// Text('شاغر',style: Styles.hintText,)
+// ],
+// )
+// ],
+// ),
