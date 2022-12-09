a060e1dfac8ad7cee0f000f754a8ef7ae9ca0a91
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-12-09 13:41:21 +0300

    Update page.dart

--- lib/app/modules/driver_pages/register/page.dart
+++ lib/app/modules/driver_pages/register/page.dart
@@ -14,79 +14,83 @@ class DriverRegisterPage extends GetView<DriverRegisterController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Palette.backgroundColor,
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.only(top: 40),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'تسجيل السائق',
-                  style: TextStyle(
-                      fontFamily: 'Tajawal',
-                      fontSize: 25,
-                      color: Palette.blueColor),
-                ),
-
-                const SizedBox(height: 20),
-
-                //
-                Container(
-                  height: 566,
-                  width: 345,
-                  decoration: BoxDecoration(
-                      color: Palette.whiteColor,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const driverPic(),
-
-                      const SizedBox(
-                        height: 20,
-                      ),
-
-                      //name filed
-                      const textFiled(
-                        headtext: 'الأسم',
-                        hinttext: 'حسن خالد ابو الخط',
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-
-                      //
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: const [
-                          dropdown(),
-                          dropdown(),
-                        ],
-                      ),
-
-                      //phone number filed
-                      const textFiled(
-                          headtext: 'رقم الهاتف', hinttext: '07712345678'),
-                      const SizedBox(
-                        height: 20,
-                      ),
-
-                      //tele user name filed
-                      textFiled(
-                          headtext: 'معرّف التلكرام', hinttext: 'khaltk.3mtk'),
-                      SizedBox(
-                        height: 20,
-                      ),
-
-                      //password filed
-                      textFiled(headtext: 'كلمة المرور', hinttext: 'خالتك'),
-                    ],
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'تسجيل السائق',
+                    style: TextStyle(
+                        fontFamily: 'Tajawal',
+                        fontSize: 25,
+                        color: Palette.blueColor),
                   ),
-                ),
-              ],
+
+                  const SizedBox(height: 20),
+
+                  //
+                  Container(
+                    height: 566,
+                    width: 345,
+                    decoration: BoxDecoration(
+                        color: Palette.whiteColor,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const driverPic(),
+
+                        const SizedBox(
+                          height: 20,
+                        ),
+
+                        //name filed
+                        const textFiled(
+                          headtext: 'الأسم',
+                          hinttext: 'حسن خالد ابو الخط',
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+
+                        //
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: const [
+                            dropdown(),
+                            dropdown(),
+                          ],
+                        ),
+
+                        //phone number filed
+                        const textFiled(
+                            headtext: 'رقم الهاتف', hinttext: '07712345678'),
+                        const SizedBox(
+                          height: 20,
+                        ),
+
+                        //tele user name filed
+                        textFiled(
+                            headtext: 'معرّف التلكرام',
+                            hinttext: 'khaltk.3mtk'),
+                        SizedBox(
+                          height: 20,
+                        ),
+
+                        //password filed
+                        textFiled(headtext: 'كلمة المرور', hinttext: 'خالتك'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
