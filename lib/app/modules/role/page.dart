e5a90e2348e8e52456fe4c0a5a1df8f5d4c5a4d5
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2023-01-02 13:06:23 +0300

    Update page.dart

--- lib/app/modules/role/page.dart
+++ lib/app/modules/role/page.dart
@@ -3,8 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:student_transport_lines/core/theme/color_theme.dart';
 import 'controller.dart';
-import 'widgets/card_driver_stud.dart';
-import 'package:student_transport_lines/core/theme/padding.dart';
 
 class RolePage extends GetView<RoleController> {
   @override
@@ -14,33 +12,35 @@ class RolePage extends GetView<RoleController> {
 
     return SafeArea(
       child: Scaffold(
-          body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/gray_taxi.png"),
-            fit: BoxFit.contain,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/gray_taxi.png"),
+              fit: BoxFit.contain,
+            ),
           ),
-        ),
-        padding: EdgeInsets.symmetric(
-            horizontal: sizew * 0.075, vertical: sizeh * 0.05),
-        child: Column(children: [
-          
-          IconButton(onPressed: (){
-            Get.toNamed('/driver/home');
-          },
-              icon: Icon(Icons.cancel_outlined)),
-          // Stepper
-          Expanded(
-            flex: 1,
-            child: SizedBox(
-                width: sizew * 0.4,
-                child: Obx(() => Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          for (MapEntry entry
-                              in controller.steps.asMap().entries)
-                            GestureDetector(
-                              child: Row(children: [
+          padding: EdgeInsets.symmetric(
+              horizontal: sizew * 0.075, vertical: sizeh * 0.05),
+          child: Column(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Get.toNamed('/driver/home');
+                  },
+                  icon: Icon(Icons.cancel_outlined)),
+              // Stepper
+              Expanded(
+                flex: 1,
+                child: SizedBox(
+                  width: sizew * 0.4,
+                  child: Obx(
+                    () => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        for (MapEntry entry in controller.steps.asMap().entries)
+                          GestureDetector(
+                            child: Row(
+                              children: [
                                 Container(
                                   alignment: Alignment.center,
                                   width: 12.0.w,
@@ -62,18 +62,23 @@ class RolePage extends GetView<RoleController> {
                                           ? Palette.yellowColor
                                           : Palette.greyColor2),
                                 ),
-                              ]),
-                              onTap: () => controller.goToStep(entry.key),
-                            )
-                        ]))),
-          ),
+                              ],
+                            ),
+                            onTap: () => controller.goToStep(entry.key),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
 
-          // Content
-          Expanded(
-            flex: 8,
-            child: Container(
-                alignment: Alignment.topRight,
-                child: Obx(() => AnimatedSwitcher(
+              // Content
+              Expanded(
+                flex: 8,
+                child: Container(
+                  alignment: Alignment.topRight,
+                  child: Obx(
+                    () => AnimatedSwitcher(
                       duration: const Duration(milliseconds: 200),
                       transitionBuilder:
                           (Widget child, Animation<double> animation) {
@@ -84,46 +89,56 @@ class RolePage extends GetView<RoleController> {
                         child: controller.steps[controller.currentStep.value],
                         key: ValueKey<int>(controller.currentStep.value),
                       ),
-                    ))),
-          ),
-
-          // Next Button
-          SizedBox(
-            width: double.infinity,
-            height: 48.0,
-            child: ElevatedButton(
-                style: const ButtonStyle(
-                  shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                          side: BorderSide(color: Colors.transparent))),
-                  shadowColor:
-                      MaterialStatePropertyAll<Color>(Colors.transparent),
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Palette.yellowColor),
+                    ),
+                  ),
                 ),
-                onPressed: () => controller.nextStep(),
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Obx(() => !controller.isLoading.value
-                      ? Text(
-                          controller.currentStep.value ==
-                                  controller.steps.length - 1
-                              ? 'حفظ'
-                              : 'التالي',
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              fontFamily: 'Tajawal'),
-                        )
-                      : const CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                              Palette.whiteColor))),
-                ])),
+              ),
+
+              // Next Button
+              SizedBox(
+                width: double.infinity,
+                height: 48.0,
+                child: ElevatedButton(
+                  style: const ButtonStyle(
+                    shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                            side: BorderSide(color: Colors.transparent))),
+                    shadowColor:
+                        MaterialStatePropertyAll<Color>(Colors.transparent),
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Palette.yellowColor),
+                  ),
+                  onPressed: () => controller.nextStep(),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Obx(
+                        () => !controller.isLoading.value
+                            ? Text(
+                                controller.currentStep.value ==
+                                        controller.steps.length - 1
+                                    ? 'حفظ'
+                                    : 'التالي',
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    fontFamily: 'Tajawal'),
+                              )
+                            : const CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                    Palette.whiteColor),
+                              ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ]),
-      )),
+        ),
+      ),
     );
   }
 }
